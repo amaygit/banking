7dbb4819b909ff121eece7e013fd477ec9210453
Amay Nayak <mailamaynayak@gmail.com>
2024-05-10 19:14:05 +0530

    small change

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import RecentTransactions from "@/components/RecentTransactions";
 import RightSidebar from "@/components/RightSidebar";
-import TotalBalanceBox from "@/components/TotalBalancebox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
 import HeaderBox from "@/components/ui/HeaderBox";
 import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
